b5261f23373b1b30729b6d1773f9f8d42a5c2c92
caren <carenoliveirasilvapontes@gmail.com>
2022-08-18 18:51:31 -0500

    requisito 9 e 10

--- src/components/MissionCard.js
+++ src/components/MissionCard.js
@@ -5,11 +5,11 @@ class MissionCard extends React.Component {
   render() {
     const { name, year, country, destination } = this.props;
     return (
-      <div data-testid="mission-card">
-        <p data-testid="mission-name">{name}</p>
-        <p data-testid="mission-year">{year}</p>
-        <p data-testid="mission-country">{country}</p>
-        <p data-testid="mission-destination">{destination}</p>
+      <div className="content-mission" data-testid="mission-card">
+        <p className="name" data-testid="mission-name">{name}</p>
+        <p className="year" data-testid="mission-year">{year}</p>
+        <p className="country" data-testid="mission-country">{country}</p>
+        <p className="destination" data-testid="mission-destination">{destination}</p>
       </div>
     );
   }
